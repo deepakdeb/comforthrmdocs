a9a7c85cd75583c519290de54ddfe3c998287a16
Sabuj Kundu <manchumahara@gmail.com>
2024-12-09 20:22:37 +0600

    trying gulp

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+import gulp from 'gulp';
+import { deleteAsync } from 'del';
+//const del = require('del');
+import { promises as fs } from 'fs';
+import path from 'path';
+
+import replace from 'gulp-replace';
+
+const dist_path = 'docs/.vitepress/dist';
+const www_path = 'www';
+
+// Copy Job files
+async function copyDist() {
+    // Clean destination folder
+    await deleteAsync([path.join(www_path, 'dist/**/*')]);
+
+    // Ensure destination directory exists
+    await fs.mkdir(path.join(www_path, 'dist'), { recursive: true });
+
+    return new Promise((resolve, reject) => {
+// Copy files from source to destination
+        gulp.src(path.join(dist_path, '**', '*'))  // Include all files and subfolders
+            .pipe(gulp.dest(path.join(www_path, 'dist')))  // Destination folder
+            .on('end', resolve)   // Resolve the Promise when the task is complete
+            .on('error', reject) // Reject the Promise if an error occurs
+            .on('error', (err) => console.error('Error during copy:', err));  // Log errors
+    });
+
+}
+
+function copyReplace() {
+    return gulp.src(path.join(dist_path, '**', '*.*'))  // Adjust the path to your directory and file types
+        .pipe(replace(/\/comforthrmdocs\//g, (match) => {
+            console.log(`Replaced: ${match}`);  // Log every replacement
+            return '';  // Replace with '/'
+        }))
+        .pipe(gulp.dest(path.join(www_path, 'dist')));  // Save the modified files to a 'dist' directory
+}
+
+
+// Define complex tasks
+const www = gulp.series(copyDist, copyReplace);
+
+export { www };  // Export task so it can be run via npm
